698fae7c8ecb409da486bc683d9565080c43c881
Jonathan Ah-Nee <jonahnee@gmail.com>
2015-12-12 16:02:04 -1000

    cleaded up code

--- timer.js
+++ timer.js
@@ -4,27 +4,16 @@ function Timer (maxTimer) {
   EventEmitter.call(this);
   var self = this;
   var intervalID = null;
-
   var interval = 0;
   maxTimer = maxTimer || 10;
-  // if (interval > maxTimer) {
-  //   self.stop();
-  // }
-
 
   this.start = function () {
-    // var interval = 0;
-    // tickCount = tickCount || 10;
     var startTime = Date.now();
     self.emit('start', {startTime: startTime});
-
     this.intervalID = setInterval(function () {
       if (interval >= maxTimer) {
         self.stop();
       } else {
-    // if (interval > tickCount) {
-    //   self.stop();
-    //   } else {
         self.emit('tick', { interval : ++interval });
       }
     },  1000);
@@ -33,7 +22,6 @@ function Timer (maxTimer) {
   this.stop = function () {
     var endTime = Date.now();
     self.emit('stop', {endTime: endTime});
-
     clearInterval(this.intervalID);
   };
 }
@@ -47,24 +35,6 @@ Timer.prototype = new Object(EventEmitter.prototype, {
 function tickHandler(event) {
   console.log(event);
 }
-
-// Timer.prototype.start = function (tickCount) {
-//   var self = this;
-//   this.i = 0;
-//   tickCount = tickCount || 60;
-//   var startTime = Date.now();
-//   self.emit('start', {startTime: startTime});
-
-//   setInterval(function () {
-//     self.emit('tick', { interval : self.i++ });
-//   }, 1000);
-
-// };
-
-// Timer.prototype.stop = function () {
-//   var endTime = Date.now();
-//   self.emit('stop', {endTime: endTime});
-// };
 
 var basicTimer = new Timer(15);
 
