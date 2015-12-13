acff331068bce45118cba4abed3ed30457654c4e
Jonathan Ah-Nee <jonahnee@gmail.com>
2015-12-12 16:00:48 -1000

    completed time limit

--- timer.js
+++ timer.js
@@ -1,20 +1,75 @@
 var EventEmitter = require('events');
 
-function Timer () {
+function Timer (maxTimer) {
   EventEmitter.call(this);
   var self = this;
-  setInterval(function () {
-    self.emit('tick');
-  }, 1000);
+  var intervalID = null;
+
+  var interval = 0;
+  maxTimer = maxTimer || 10;
+  // if (interval > maxTimer) {
+  //   self.stop();
+  // }
+
+
+  this.start = function () {
+    // var interval = 0;
+    // tickCount = tickCount || 10;
+    var startTime = Date.now();
+    self.emit('start', {startTime: startTime});
+
+    this.intervalID = setInterval(function () {
+      if (interval >= maxTimer) {
+        self.stop();
+      } else {
+    // if (interval > tickCount) {
+    //   self.stop();
+    //   } else {
+        self.emit('tick', { interval : ++interval });
+      }
+    },  1000);
+  };
+
+  this.stop = function () {
+    var endTime = Date.now();
+    self.emit('stop', {endTime: endTime});
+
+    clearInterval(this.intervalID);
+  };
 }
 
 Timer.prototype = new Object(EventEmitter.prototype, {
   constructor: {
-    value: EventEmitter,
+    value: EventEmitter
   }
 });
 
-var basicTimer = new Timer();
-basicTimer.addListener('tick', function (event) {
-  process.stdout.write('tick \n');
-});
+function tickHandler(event) {
+  console.log(event);
+}
+
+// Timer.prototype.start = function (tickCount) {
+//   var self = this;
+//   this.i = 0;
+//   tickCount = tickCount || 60;
+//   var startTime = Date.now();
+//   self.emit('start', {startTime: startTime});
+
+//   setInterval(function () {
+//     self.emit('tick', { interval : self.i++ });
+//   }, 1000);
+
+// };
+
+// Timer.prototype.stop = function () {
+//   var endTime = Date.now();
+//   self.emit('stop', {endTime: endTime});
+// };
+
+var basicTimer = new Timer(15);
+
+basicTimer.addListener('tick', tickHandler);
+basicTimer.addListener('start', tickHandler);
+basicTimer.addListener('stop', tickHandler);
+
+basicTimer.start();
